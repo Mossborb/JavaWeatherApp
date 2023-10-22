48e8db20c782d1c8272795d8cd139bda3006a48c
MossBorb <Mossborb@gmail.com>
2023-10-22 11:52:21 -0400

    Created switch statements to ask user which day they'd like the weather for.

--- WeatherApp.java
+++ WeatherApp.java
@@ -1,9 +1,11 @@
 import java.util.Random;
+import java.util.Scanner;
 
-public class Main {
+public class WeatherApp {
     public static void main(String[] args) {
 
         Random random = new Random();
+        Scanner userInp = new Scanner(System.in);
 
         int SundayWeather;
         int MondayWeather;
@@ -21,17 +23,44 @@ public class Main {
         FridayWeather = random.nextInt(101);
         SaturdayWeather = random.nextInt(101); 
 
+        
+
+        System.out.println("What day would you like the temperature for?");
+        String Day = userInp.nextLine();
+
+        switch(Day) {
+            case "Sunday": System.out.println("Sundays Temperature:    " + SundayWeather+"F");
+            break;
+            case "Monday": System.out.println("Mondays Temperature:    " + MondayWeather+"F");
+            break;
+            case "Tuesday": System.out.println("Tuesdays Temperature:   " + TuesdayWeather+"F");
+            break;
+            case "Wednesday": System.out.println("Sundays Temperature:    " + WednesdayWeather+"F");
+            break;
+            case "Thursday": System.out.println("Sundays Temperature:    " + ThursdayWeather+"F");
+            break;
+            case "Friday": System.out.println("Sundays Temperature:    " + FridayWeather+"F");
+            break;
+            case "Saturday": System.out.println("Sundays Temperature:    " + SaturdayWeather+"F");
+            break;
+            default: System.out.println("That is not a valid day...");
 
 
-        System.out.println("Sundays Temperature:    " + SundayWeather+"F");
-        System.out.println("Mondays Temperature:    " + MondayWeather+"F");
-        System.out.println("Tuesdays Temperature:   " + TuesdayWeather+"F");
+        }
+
+
+/*
+        
+        
+        
         System.out.println("Wednesdays Temperature: " + WednesdayWeather+"F");
         System.out.println("Thursdays Temperature:  " + ThursdayWeather+"F");
         System.out.println("Fridays Temperature:    " + FridayWeather+"F");
         System.out.println("Saturdays Temperature:  " + SaturdayWeather+"F");
 
         System.out.println();
+*/
+        
         
         
         
